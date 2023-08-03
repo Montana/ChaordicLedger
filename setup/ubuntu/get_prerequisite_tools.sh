c4502341a0afa16b89115af2e3a8f37e5313ca08
Michael Mendy <montana@linux.com>
2023-08-03 10:32:03 -0700

    adding `build-essential` makes this a lot easier to work with if on Debian

--- setup/ubuntu/get_prerequisite_tools.sh
+++ setup/ubuntu/get_prerequisite_tools.sh
@@ -5,6 +5,7 @@ sudo apt-get -y update
 # Note: libdigest-sha-perl contains the 'shasum' tool.
 sudo apt-get -y install \
                 git \
+                build-essential \
                 ca-certificates \
                 curl \
                 gnupg \
